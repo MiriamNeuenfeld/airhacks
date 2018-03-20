cd14e3422b9a3a06cde339b3481e8a070100793c
Adam Bien <abien@adam-bien.com>
2018-03-20 10:52:19 +0100

    service worker is able to access the cache

--- service-workers/src/swworker.js
+++ service-workers/src/swworker.js
@@ -1,10 +1,23 @@
 //console.log(this);
 //console.log(self);
 //
+
 self.onactivate = e => console.log('activate', e);
-self.oninstall = e => console.log('install ');
-self.onfetch = e => { 
-
-    console.log('request is: ',e.request);
-
+self.oninstall = e => { 
+    e.waitUntil(
+        caches.open('payload-cache').
+            then(c => c.add('payload.json'))
+    );
 }
+self.onfetch = e => {
+    if (e.request.url.endsWith('.json')) {
+        e.respondWith(
+            caches.open('payload-cache').
+                then(c => c.match(e.request))
+        )
+    }
+}
+            
+    
+
+
